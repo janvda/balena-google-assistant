f4c717f5ecdf385e09fb91f9c5d8d988a994efce
janvda <jan@Jans-MBP.lan>
2019-06-22 12:14:54 +0200

    show the contents of the generated credentials

--- google_assistant/create_credentials.sh
+++ google_assistant/create_credentials.sh
@@ -4,3 +4,7 @@ source /env/bin/activate
 google-oauthlib-tool --scope https://www.googleapis.com/auth/assistant-sdk-prototype \
                      --scope https://www.googleapis.com/auth/gcm \
                      --save --headless --client-secrets /client_secret.json 
+
+echo "The credentials ="
+cat /root/.config/google-oauthlib-tool/credentials.json
+echo ""
